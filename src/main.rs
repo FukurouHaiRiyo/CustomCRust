85eec6512235e7ddf122a4de7e1f583b165e7d55
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-06-04 18:03:37 +0300

    updates

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,5 @@ mod math;
 use print::p::*;
 
 fn main() {
-    // let mut line = String::new();
-    // println!(
-    //     "Enter your name: "
-    // );
-    // std::io::stdin().read_line(&mut line).unwrap();
-    // let name = line.trim();
-
-    // printf("Your name: %s", &[&name]);
-
-    // println!("{}", math::abs::abs(-2));
-
-    // println!("{}", math::aliquot_sum::aliquot_sum(6));
-
-    // println!("{:?}", math::amicable_nums::amicable_nums(1000));
-
     
 }
--- src/math/atio.rs
+++ src/math/atio.rs
@@ -4,4 +4,24 @@ fn ft_isspace(c: char) -> bool {
 
 pub fn atoi(s: &str) -> i32 {
     let bytes = s.as_bytes();
+    let mut i = 0;
+    let len = bytes.len();
+    let mut sign = 1;
+    let mut result = 0;
+
+    // skip leading whitespaces
+    while i < len && ft_isspace(bytes[i] as chat) {
+        i += 1;
+    }
+
+    // check for sign
+    if i < len && (bytes[i] == b'-' || bytes[i] == b'+') {
+        if bytes[i] == b'-' {
+            sign = -1;
+        }
+
+        i += 1;
+    }
+
+    sign * result
 }
