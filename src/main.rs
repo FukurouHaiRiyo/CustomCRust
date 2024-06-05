d63ce13ebbdb565f7aa89dcae1289baea5756116
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-06-05 18:57:31 +0300

    updates

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod print;
 mod math;
 
 use print::p::*;
-use math::c_func::atoi;
+use math::c_func::{atoi, itoa};
+use math::abs::abs;
 
 fn main() {
     println!("{}", atoi(" -123"));
+    println!("{}", abs(-2));
+    // println!("{:?}", itoa(-123));
 }
--- src/math/c_func.rs
+++ src/math/c_func.rs
@@ -1,4 +1,7 @@
-use std::alloc::{alloc, Layout};
+use crate::abs;
+
+
+use std::alloc::{alloc as all, Layout};
 use std::ptr;
 
 fn isspace(c: char) -> bool {
@@ -7,6 +10,74 @@ fn isspace(c: char) -> bool {
 
 fn isalnum(c: char) -> bool {
     (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
+}
+
+fn is_alpha(c: char) -> bool {
+    (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+}
+
+fn is_ascii(c: i32) -> bool {
+    c >= 0 && c<= 127
+}
+
+fn is_digit(c: char) -> bool {
+    c >= '0' && c <= '9'
+}
+
+fn is_print(c: i32) -> bool {
+    c >= 32 && c <= 126
+}
+
+fn is_space(c: char) -> bool {
+    (c >= '\u{0009}' && c <= '\u{000D}') || c == ' '
+}
+
+fn len_num(mut n: i32) -> usize {
+    let mut len = 0;
+    if n <= 0 {
+        len += 1;
+    }
+
+    while n != 0 {
+        n /= 10;
+        len += 1;
+    }
+
+    len
+}
+
+pub fn itoa(n: i32) -> Option<String> {
+    let len = len_num(n);
+    let mut ret = unsafe { all(Layout::array::<u8>(len + 1).unwrap()) } as *mut u8;
+
+    if ret.is_null() {
+        return None;
+    }
+
+    let mut n = n;
+
+    unsafe {
+        ret = ret.offset(len as isize);
+        *ret = b'\0';
+        if n == 0 {
+            *ret.offset(-1) = b'0';
+        } else if n < 0 {
+            *ret.offset(-1) = b'-';
+            n = -n;
+        }
+
+        while n != 0 {
+            ret = ret.offset(-1);
+            *ret = (abs(n % 10) as u8) + b'0';
+            n /= 10;
+        }
+    }
+
+    let s = unsafe {
+        String::from_utf8_unchecked(Vec::from_raw_parts(ret, len + 1, len + 1))
+    };
+
+    Some(s)
 }
 
 pub fn atoi(s: &str) -> i32 {
@@ -54,7 +125,7 @@ fn alloc(count: usize, size: usize) -> *mut u8 {
     let total_size = count * size;
     let layout = Layout::from_size_align(total_size, 1).expect("Failed to create layout");
 
-    let ret = unsafe {alloc(layout)};
+    let ret = unsafe {all(layout)};
     if ret.is_null() {
         return ptr::null_mut();
     }
