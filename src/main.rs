db0bbfce78f7b814b9726fccf609186bd5542eb5
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-06-10 15:15:37 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,35 @@ fn main() {
     println!("{}", atoi(" -123"));
 
     // test for abs
-    println!("Abs test: abs(-2): {}", abs(-2));
+    println!("Abs test: {}", abs(-2));
 
     // test for itoa
     // println!("{:?}", itoa(-123));
 
     // test for is_digit
-    println!("{}", is_digit('1'));
+    println!("is_digit test: {}", is_digit('1'));
 
     // test for substr
     let s = "1234567890";
     let result = substr(s, 4, 8);
-    println!("Result: {}", result);
+    println!("substr test: {}", result);
 
     // test for strcat
     let s1 = strcat(&[&"Garden", &" of ", &"Eden", &'.']);
     let s2 = strcat(&[&s1, &" Will"]);
-    println!("{}, {}", s1, s2);
+    println!("strcat test: {}, {}", s1, s2);
 
     // test for name_of()
-    println!("{}", (&s2).name_of());
+    println!("name_of test: {}", (&s2).name_of());
 
     // test for sublist
     let list = vec![1, 2, 3, 4, 5];
-    println!("{:?}", sublist(&list, 2, 4));
+    println!("sublist test: {:?}", sublist(&list, 2, 4));
 
     // test for listcat
     let list1 = vec![6, 7, 8];
     let reuslt = listcat!(list, list1);
-    println!("{:?}", result);
+    println!("listcat test: {:?}", result);
 
     // test for sin 
     let angle: f64 = 1.0;
